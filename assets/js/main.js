283f1c7a314d9260d5c8ba3bd1558fda5d9cf15a
Christian Dimitri <christian.j.dimitrii@gmail.com>
2020-04-01 01:07:12 +0200

    Interface in progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,10 +43,10 @@
       item.style.backgroundImage = "url('" + url + "')";
     }
 
-    // dropdown menu
-    // var dropdown = document.querySelector('.dropdown');
-    // dropdown.addEventListener('click', function (event) {
-    //   event.stopPropagation();
-    //   dropdown.classList.toggle('is-active');
-    // });
+    //dropdown menu
+    var dropdown = document.querySelector('.dropdown');
+    dropdown.addEventListener('click', function (event) {
+      event.stopPropagation();
+      dropdown.classList.toggle('is-active');
+    });
   });
